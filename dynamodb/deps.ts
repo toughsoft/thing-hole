0ff52f1b784832dc122e331097825ea834407c92
Sean Fridman <mail@seanfridman.com>
2024-03-04 11:59:08 -0500

    chore: bump dynamodb client

--- dynamodb/deps.ts
+++ dynamodb/deps.ts
@@ -2,4 +2,4 @@ export type {
   DynamodbClient,
   DynamodbClientOptions,
   DynamoObject,
-} from "https://raw.githubusercontent.com/toughsoft/dynamodb-client/1.2.0/mod.ts";
+} from "https://raw.githubusercontent.com/toughsoft/dynamodb-client/1.2.1/mod.ts";
